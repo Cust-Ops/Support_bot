7bb5608276b1723c4319c775e5dd0c669adad25c
PunkBishop <78013550+PunkBishop@users.noreply.github.com>
2021-04-12 10:53:04 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,2 @@
+token_bot = '1721490273:AAFfuOmgC_nQWmhaLTyKmsND11EjMWwCNTc'
+chat_id = '-1001255614977'
---
+++ main.py
@@ -0,0 +1,34 @@
+import telebot
+import config  # Переменные
+bot = telebot.TeleBot(config.token_bot)
+keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
+keyboard1.row('Зарегистрировать обращение')
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.from_user.id, 'Используйте клавиатуру для навигации по функциям бота.',
+                     reply_markup=keyboard1)
+
+
+@bot.message_handler(content_types=['text'])
+def text_messages(message):
+    if message.text.lower() == "зарегистрировать обращение":
+        print(f'Обращение от пользователя Логин: @{message.from_user.username}\nИмя: {message.from_user.first_name}'
+              f' {message.from_user.last_name}')
+        send = bot.send_message(message.from_user.id, "Опишите свою проблему")
+        bot.send_message(config.chat_id, f'Поступило обращение в поддержу от '
+                                         f'{message.from_user.first_name} {message.from_user.last_name}, '
+                                         f'ожидайте последующего сообщения с информацией')
+        bot.register_next_step_handler(send, help_request)
+
+
+def help_request(message):
+    print(f'Текст обращения {message}')
+    user_name = f'Логин: @{message.from_user.username} имя: {message.from_user.first_name}' \
+                f' {message.from_user.last_name}'
+    bot.send_message(message.from_user.id, 'Ваша заявка принята, ожидайте ответа от IT отдела')
+    bot.send_message(config.chat_id, f'{user_name} отправил запрос: {message.text}')
+
+
+bot.infinity_polling()
